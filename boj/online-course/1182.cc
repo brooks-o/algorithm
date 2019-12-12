d312fe85e044098e0fe85f2cb8362fc871768cfe
machinehuman <dhwltn02@gmail.com>
2019-12-13 01:20:53 +0900

    브루트 포스 - N과 M(2)

--- boj/online-course/1182.cc
+++ boj/online-course/1182.cc
@@ -7,31 +7,35 @@ using ll = long long;
 using namespace std;
 
 int a[20];
-int n,m;
+int n, m;
 int ans = 0;
 
-void solve(int index, int sum){
-    if(index == n){
+void solve(int index, int sum)
+{
+    if (index == n)
+    {
         if (sum == m)
         {
             ans += 1;
         }
         return;
     }
-    solve(index+1, sum+a[index]);
-    solve(index+1, sum);
+    solve(index + 1, sum + a[index]);
+    solve(index + 1, sum);
 }
 
 int main()
-{   
+{
     cin >> n >> m;
-    for(int i =0; i<n; i++){
+    for (int i = 0; i < n; i++)
+    {
         cin >> a[i];
     }
-    solve(0,0);
+    solve(0, 0);
 
-    if (m==0) ans += -1;
-    
+    if (m == 0)
+        ans += -1;
+
     cout << ans << '\n';
 
     return 0;
