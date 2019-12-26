9c7889ff3bcdd43f933b9c4860d7b26e5b64de44
machine-human <dhwltn02@gmail.com>
2019-12-26 12:48:49 +0900

    다이나믹 프로그래밍 - 2xN 타일링 2

---
+++ boj/online-course/11727.cc
@@ -0,0 +1,23 @@
+#include <cstdio>
+using ll = long long;
+
+using namespace std;
+
+int d[100001];
+
+int main()
+{
+    d[0] = 1;
+    d[1] = 1;
+    int n;
+    scanf("%d", &n);
+
+    for (int i = 2; i <= n; i++)
+    {
+        d[i] = d[i - 1] + d[i - 2] + d[i - 2];
+        d[i] %= 10007;
+    }
+
+    printf("%d\n", d[n]);
+    return 0;
+}
